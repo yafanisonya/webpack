2313001b44e3394806d37f66aa0c26d7706c778a
yafanisonya <yafanison@gmail.com>
2020-02-15 15:38:37 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
     // filename: 'index.js'
-    filename: '[name].[contenthash].js',
+    filename: 'main.[contenthash].js',
+  },
+  plugins: [new HtmlWebpackPlugin(
+    {
+      title: 'fanison',
+      template: 'src/assets/index.html'
+    }
+  )],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   }
 };
+
+
