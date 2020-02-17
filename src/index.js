c20ee71e483724fe90a503b72627dd77ffdf042d
yafanisonya <yafanison@gmail.com>
2020-02-17 17:29:09 +0800

    update

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ console.log("hi ")
 
 const div = document.getElementById('app')
 
-console.log(png)
-div.innerHTML = `
-  <img src="${png}">
-`
+// console.log(png)
+// div.innerHTML = `
+//   <img src="${png}">
+// `
 const button = document.createElement('button')
 button.innerText = "懒加载"
-button.onclick = ()=>{
+button.onclick = () => {
   const promise = import('./lazy')
-  promise.then((module)=>{
+  promise.then((module) => {
     const fn = module.default
     fn()
-  },()=>{
+  }, () => {
     console.log('加载模错误')
   })
 }
